c50a8a21612c202bdcaecb657749f79cc449459f
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-02-28 15:13:28 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(limit)
+  i=1
+  multiples = [] #for some reason it only works when multiples is defined here in the local scope, not in the global scope before the method declaration. Is that a ruby feature? 
+  while i < limit do
+    if i % 3 == 0 || i % 5 == 0
+      multiples.push(i)
+    end
+    i+=1
+  end
+  return multiples
+end
+
+def sum_multiples(limit)
+  new_array=collect_multiples(limit)
+  new_array.reduce(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit=limit
+  end
+  def collect_multiples
+    i=1
+    @multiples=[]
+    while i < @limit do
+      if i % 3 == 0 || i % 5 == 0
+        @multiples.push(i)
+      end
+      i+=1
+    end
+    return @multiples
+  end
+
+  def sum_multiples
+    new_array = collect_multiples
+    new_array.reduce(0, :+)
+  end
+end
